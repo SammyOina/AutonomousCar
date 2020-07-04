274c39b29cf9cd2c1440cc00692c8a8a20226a75
sammyoina <sammtoina@gmail.com>
2020-07-04 13:22:20 +0100

    motor driver

---
+++ motor.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+def init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+
+def reverse(sec):
+    init()
+    GPIO.output(17, True)
+    GPIO.output(22, True)
+    GPIO.output(23, True)
+    GPIO.output(24, False)
+    sleep(sec)
+    GPIO.cleanup()
+
+def forward(sec):
+    init()
+    GPIO.output(17, True)
+    GPIO.output(22, True)
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    sleep(sec)
+    GPIO.cleanup()
+
+def left(sec):
+    init()
+    GPIO.output(17, False)
+    GPIO.output(22, True)
+    GPIO.output(23, True)
+    GPIO.output(24, True)
+    sleep(sec)
+    GPIO.cleanup()
+def right(sec):
+    init()
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    GPIO.output(23, True)
+    GPIO.output(24, True)
+    sleep(sec)
+    GPIO.cleanup()
+def rightfor(sec):
+    init()
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    GPIO.output(23, False)
+    GPIO.output(24, True)
+    sleep(sec)
+    GPIO.cleanup()
+    
+rightfor(0.5)
